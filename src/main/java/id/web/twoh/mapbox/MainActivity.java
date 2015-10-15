865d220b38cfe581f75e30fcdb4247edb9239d52
twoh <hherdinaufal@gmail.com>
2015-10-15 15:01:47 +0700

    first sample commit

---
+++ src/main/java/id/web/twoh/mapbox/MainActivity.java
@@ -0,0 +1,70 @@
+package id.web.twoh.mapbox;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.overlay.Icon;
+import com.mapbox.mapboxsdk.overlay.Marker;
+import com.mapbox.mapboxsdk.overlay.UserLocationOverlay;
+import com.mapbox.mapboxsdk.views.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        setMap();
+    }
+
+    private void setMap(){
+        MapView mapView = (MapView) findViewById(R.id.mapview);
+        Marker marker = new Marker("Kementerian BUMN","Kantor",new LatLng(-6.181793, 106.825434)).setIcon(new Icon(this, Icon.Size.MEDIUM,"commercial","#990000"));
+        mapView.addMarker(marker);
+        mapView.setDiskCacheEnabled(true);
+        mapView.setUserLocationEnabled(true)
+                .setUserLocationTrackingMode(UserLocationOverlay.TrackingMode.NONE);
+        mapView.getController().setZoomAnimated(13f, new LatLng(-6.178696, 106.827722), true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
